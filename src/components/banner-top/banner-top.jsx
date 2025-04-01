b71033918354b014e6a08433934e32e4a4b91d32
rafainsunza <rafainsunza@outlook.com>
2025-04-01 20:23:25 +0200

    Schedule banner styling

--- src/components/banner-top/banner-top.jsx
+++ src/components/banner-top/banner-top.jsx
@@ -10,26 +10,53 @@ const BannerTop = ({
 }) => {
   const { translations } = useContext(LanguageContext);
   const [titleText, setTitleText] = useState("");
+  const [buttonText, setButtonText] = useState({
+    previous: "",
+    next: "",
+  });
 
   useEffect(() => {
     // wait for translations before rendering
     if (translations?.banner_top) {
       setTitleText(translations?.banner_top?.[titleKey]);
     }
+
+    if (hasButtons && translations?.banner_top) {
+      setButtonText({
+        previous: translations?.banner_top?.previous,
+        next: translations?.banner_top?.next,
+      });
+    }
   }, [translations, titleKey]);
 
   return (
     <div className="banner-top-outer">
       <div className="banner-top-wrapper">
-        <h1 className="banner-top-title">
-          {titleText.toUpperCase()}
+        <div className="banner-top-titles">
+          <h1 className="banner-top-title">
+            {titleText.toUpperCase()}&nbsp;
+            {hasTitleSpan && <span className="banner-top-title-span"> 25</span>}
+          </h1>
 
-          {hasTitleSpan && <span className="banner-top-title-span"></span>}
-        </h1>
+          {hasSecondaryTitle && (
+            <h2 className="banner-top-secondary-title">
+              20 DECEMBER T/M 28 DECEMBER 2025
+            </h2>
+          )}
+        </div>
 
-        {hasSecondaryTitle && <h2 className="banner-top-secondary-title"></h2>}
-
-        {hasButtons && <div className="banner-top-buttons"></div>}
+        {hasButtons && (
+          <div className="banner-top-buttons">
+            <button className="banner-top-buttons-previous">
+              <i className="fa-solid fa-caret-left"></i>{" "}
+              {buttonText.previous.toUpperCase()}
+            </button>
+            <button className="banner-top-buttons-next">
+              {buttonText.next.toUpperCase()}
+              <i className="fa-solid fa-caret-right"></i>{" "}
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
